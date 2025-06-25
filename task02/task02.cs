3dd578a5e4b2d37abf493a99202f1c8ed60cce74
ari-ira17 <ari.ira17@yandex.ru>
2025-06-25 18:21:03 +0600

    feat: add student service implimentation

--- task02/task02.cs
+++ task02/task02.cs
@@ -1,6 +1,48 @@
 ﻿namespace task02;
 
-public class Class1
+public class Student
 {
-    //
+    public string Name { get; set; } = string.Empty;
+    public string Faculty { get; set; } = string.Empty;
+    public List<int> Grades { get; set; } = new();
+}
+
+
+public class StudentService
+{
+    private readonly List<Student> _students;       
+
+    public StudentService(List<Student> students) => _students = students;
+
+    public IEnumerable<Student> GetStudentsByFaculty(string faculty)    
+    =>  _students
+        .Where(student => student.Faculty == faculty); 
+
+    public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade)        
+    => _students
+        .Where(student => student.Grades != null && 
+                   student.Grades.Count > 0 && 
+                   student.Grades.Average() >= minAverageGrade);
+
+    public IEnumerable<Student> GetStudentsOrderedByName()   
+    => _students
+        .Where(student => student.Name != null && 
+                   student.Name != "")
+        .OrderBy(student => student.Name);
+
+    public ILookup<string, Student> GroupStudentsByFaculty()      
+        => _students
+            .ToLookup(student => student.Faculty);
+
+    public string? GetFacultyWithHighestAverageGrade()        
+        => _students
+        .Where(student => student.Grades.Any()) 
+        .GroupBy(student => student.Faculty)                      
+        .Select(group => new
+        {
+            Faculty = group.Key,
+            Average = group.Average(student => student.Grades.Average()) 
+        })
+        .OrderByDescending(element => element.Average)            
+        .FirstOrDefault()?.Faculty;        
 }
