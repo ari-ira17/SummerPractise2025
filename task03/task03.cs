fd44e8a8d9398d05057964194bf960631f04d751
ari-ira17 <ari.ira17@yandex.ru>
2025-06-26 14:13:33 +0600

    feat: add custom collection implementation

--- task03/task03.cs
+++ task03/task03.cs
@@ -1,6 +1,35 @@
-﻿namespace task03;
+﻿using System.Collections;
 
-public class Class1
+namespace task03;
+
+public class CustomCollection<T> : IEnumerable<T>
 {
-    //
+    private readonly List<T> _items = new();
+
+    public void Add(T item) => _items.Add(item);
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();    
+
+    public IEnumerable<T> GetReverseEnumerator()
+    {
+        for (int i = _items.Count - 1; i >= 0; i--)
+        {
+            yield return _items[i];
+        }
+    }
+
+    public static IEnumerable<int> GenerateSequence(int start, int count)
+    {
+        for (int j = 0; j < count; j++)
+        {
+            yield return start + j;
+        }
+    }
+
+    public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+    {
+        return _items
+            .Where(predicate)
+            .OrderBy(keySelector);
+    }
 }
