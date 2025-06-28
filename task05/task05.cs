a6ee1de25b45fa7bd8d2a29a0dfcd5b6c3333d7c
ari-ira17 <ari.ira17@yandex.ru>
2025-06-28 13:00:57 +0600

    feat: add class analyzer implementation

--- task05/task05.cs
+++ task05/task05.cs
@@ -1,6 +1,49 @@
-﻿namespace task05;
+﻿using System.Reflection;
 
-public class Class1
+namespace task05;
+
+public class ClassAnalyzer
 {
-    //
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()       
+    {
+        return _type
+                .GetMethods()
+                .Where(member => member.IsPublic)
+                .Select(member => member.Name);
+    }
+
+    public IEnumerable<string> GetMethodParams(string methodname)      
+    {
+        return _type
+                .GetMethod(methodname)?
+                .GetParameters()
+                .Select(member => member.Name)!;
+    }
+
+    public IEnumerable<string> GetAllFields()     
+    {
+        return _type
+                .GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static)
+                .Select(member => member.Name);
+    }
+
+    public IEnumerable<string> GetProperties()  
+    {
+        return _type
+                .GetProperties()
+                .Select(member => member.Name);
+    }
+
+    public bool HasAttribute<T>() where T : Attribute       
+    {
+        return _type
+                .GetCustomAttribute<T>() != null;
+    }
 }
