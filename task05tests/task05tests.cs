a31486de662efda9e3286ee21665e00fdf9d14df
ari-ira17 <ari.ira17@yandex.ru>
2025-06-28 13:01:15 +0600

    feat: add tests

--- task05tests/task05tests.cs
+++ task05tests/task05tests.cs
@@ -1,10 +1,63 @@
-namespace task05tests;
+using task05;
+using Xunit;
 
-public class UnitTest1
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField = string.Empty;        
+    public int Property { get; set; }
+
+    public void Method() { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
 {
     [Fact]
-    public void Test1()
+    public void GetPublicMethods_ReturnsCorrectMethods()
     {
-        //
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
+
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetMethodParams_ReturnsCorrectParams()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var result = analyzer.GetMethodParams(nameof(TestClass.Method));
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+    }
+
+    [Fact]
+    public void GetProperties_ReturnsCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var result = analyzer.GetProperties();
+
+        Assert.Contains("Property", result);
+    }
+    
+
+    [Fact]
+    public void HasAttribute_ReturnsCorrectAnswer()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        var result = analyzer.HasAttribute<SerializableAttribute>();
+
+        Assert.True(result);
     }
 }
