4feb952a837ccf08387d95734a8bf495790966c5
ari-ira17 <ari.ira17@yandex.ru>
2025-07-09 14:19:57 +0600

    feat: add classes and date converter implementation

--- task13/task13.cs
+++ task13/task13.cs
@@ -1,6 +1,33 @@
-﻿namespace task13;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
-public class Class1
+namespace task13;
+
+public class Subject
 {
+  public string? Name { get; set; }
+  public int Grade { get; set; }
+}
 
+public class Student
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public List<Subject>? Grades { get; set; }
+}
+
+public class DateConverter : JsonConverter<DateTime>
+{
+    private const string Format = "dd.MM.yyyy";
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.ParseExact(reader.GetString()!, Format, null);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(Format));
+    }
 }
